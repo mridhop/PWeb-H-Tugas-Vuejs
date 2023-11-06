4c689793aad9b00b5a312ff1b886e01db1981a24
Ridho Pratama <dhoable@gmail.com>
2023-11-06 10:44:07 +0700

    push

---
+++ src/components/Calculator.vue
@@ -0,0 +1,42 @@
+<script setup>
+    import { ref } from 'vue';
+    
+    const num1 = ref(0);
+    const num2 = ref(0);
+    const result = ref(0);
+
+    function add() {
+        result.value = num1.value + num2.value;
+    }
+
+    function subtract() {
+        result.value = num1.value - num2.value;
+    }
+
+    function multiply() {
+        result.value = num1.value * num2.value;
+    }
+
+    function divide() {
+        result.value = num1.value / num2.value;
+    }
+</script>
+
+<template>
+    <div>
+        <h1>Calculator</h1>
+    </div>
+    <div>
+        <input v-model="num1"/>
+        <input v-model="num2"/>
+    </div>
+    <div>
+        <button v-on:click="result=parseFloat(num1)+parseFloat(num2)">+</button>
+        <button v-on:click="subtract">-</button>
+        <button @click="multiply">*</button>
+        <button @click="divide">/</button>
+    </div>
+    <div>
+        <h2>Result: {{ result }}</h2>
+    </div>
+</template>
---
+++ src/components/Tugas1.vue
@@ -0,0 +1,29 @@
+<script setup>
+    import { ref } from 'vue';
+
+    const items = ref([
+        { id: 1, text: 'Pemrograman Web - Ridho Rahman', done: true },
+        { id: 2, text: 'Sistem Deteksi Intrusi - Hatma Surotrisongko', done: true },
+        { id: 3, text: 'Smart City - M Husni', done: true },
+        { id: 4, text: 'Internet of Things - M Husni', done: true }
+    ]);
+</script>
+
+<template>
+    <div>
+        <h1>Hello</h1>
+    </div>
+    <div>
+        <ul>
+            <li v-for="item in items" :key="item.id" @click="item.done = !item.done" :style="{ backgroundColor: item.done ? 'aqua' : 'white' }">
+                {{ item.text }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+    li {
+        margin-bottom: 5px;
+    }
+</style>
